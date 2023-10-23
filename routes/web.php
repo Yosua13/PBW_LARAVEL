a336775aa5e57267091822478c3dc7dda4cfc505
Yosua Reynaldi Manurun <reyyosua29@gmail.com>
2023-10-23 17:01:32 +0700

    commit image

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,6 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
